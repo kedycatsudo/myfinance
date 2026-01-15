13fe4a8d05a3bdb856d73c905e8ef8f23253a22e
kedycat <dkocausta34@gmail.com>
2026-01-15 14:04:29 -0500

    going to start for a EditModal branchj

--- src/components/sourcesDetailsContainer/sourcesDetailsContainer.tsx
+++ src/components/sourcesDetailsContainer/sourcesDetailsContainer.tsx
@@ -1,8 +1,5 @@
 'use client';
-
 import { useState } from 'react';
-import SourceContainer from './sourceContainer';
-import { FinancePayment } from '@/types/finance';
 
 type SourcesDetailsContainerProps<T extends { id: string }> = {
   header: string;
@@ -19,13 +16,9 @@ export default function SourcesDetailsContainer<T extends { id: string }>({
   return (
     <div className="flex flex-col rounded bg-[#989899] opacity-75 items-center gap-2 px-1">
       <h1 className="text-2xl xs:text-3xl text-[#29388A] font-bold">{header}</h1>
-      <span className="w-full mt-0.5 bg-[#1E1552] bg-opacity-60 border border-[#29388A] rounded px-2 py-0.5 font-bold text-[#a9deff] text-s xs:text-xl shadow-inner"></span>
       {items.map((item) =>
         renderSource(item, !!openSources[item.id], () =>
-          setOpenSources((prev) => ({
-            ...prev,
-            [item.id]: !prev[item.id],
-          })),
+          setOpenSources((prev) => ({ ...prev, [item.id]: !prev[item.id] })),
         ),
       )}
     </div>
